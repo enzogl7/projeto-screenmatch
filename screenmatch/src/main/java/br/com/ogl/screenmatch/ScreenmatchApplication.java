7e5e75d8bbda6adebc78a93b19658240d1cc1e19
Enzo Lima <enzolima527@gmail.com>
2024-04-16 17:57:53 -0300

    criacao projeto

---
+++ screenmatch/src/main/java/br/com/ogl/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,28 @@
+package br.com.ogl.screenmatch;
+
+import br.com.ogl.screenmatch.model.DadosSerie;
+import br.com.ogl.screenmatch.service.ConsumoApi;
+import br.com.ogl.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=94b83893");
+		// System.out.println(json);
+
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+
+	}
+}
